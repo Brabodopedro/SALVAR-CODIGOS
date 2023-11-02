18d4100249ccecb53bd2d31f32af81f039abb52f
Pedro Henrique Brito Obara <106891475+Brabodopedro@users.noreply.github.com>
2023-11-02 13:51:56 -0400

    Delete binario em decimal .py

--- binario em decimal .py
+++
@@ -1,8 +0,0 @@
-# recebe o número binário como input do usuário
-binario = input("Digite um número binário: ")
-
-# converte o número binário em decimal usando a função int() com o argumento 2
-decimal = int(binario, 2)
-
-# imprime o resultado
-print(f"O número binário {binario} é igual ao número decimal {decimal}")
